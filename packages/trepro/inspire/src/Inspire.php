bfb02bf95e73c924abda4079f0a6e4900b6f5944
Tr-87pRo <126872941+Tr-87pRo@users.noreply.github.com>
2024-09-05 11:11:34 +0200

    Working Style and better look

--- packages/trepro/inspire/src/Inspire.php
+++ packages/trepro/inspire/src/Inspire.php
@@ -3,13 +3,34 @@
 namespace trepro\Inspire;
 
 use Illuminate\Support\Facades\Http;
+use Exception;
 
 class Inspire {
     public function justDoIt() {
-        $response = Http::get('https://zenquotes.io/api/random');
+        try {
+            $output = '';
 
-        dd($response->json());
+            for ($i = 0; $i < 5; $i++) {
+                $response = Http::get('https://zenquotes.io/api/random');
 
-        return $response['quote'] . ' -' . $response['author'];
+                $data = $response->json();
+
+                if (!$data || !isset($data[0]['q'], $data[0]['a'])) {
+                    throw new Exception('Invalid API response');
+                }
+
+                $quote = $data[0]['q'];
+                $author = $data[0]['a'];
+
+                $output .= "<blockquote style='background-color: #f9f9f9; padding: 20px; border-left: 5px solid #ccc;'>";
+                $output .= "<p>$quote</p>";
+                $output .= "<cite>$author</cite>";
+                $output .= "</blockquote><br>"; 
+            }
+
+            return $output;
+        } catch (Exception $e) {
+            return 'Error: ' . $e->getMessage();
+        }
     }
 }
